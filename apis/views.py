c134fdd6bf9f9c0722699676129fb37a3fde708c
Naveen Arora <naveenarorakrnl@gmail.com>
2020-03-13 18:26:49 +0530

    How to Create a basic API using Django Rest Framework ?

---
+++ apis/views.py
@@ -0,0 +1,14 @@
+# import viewsets
+from rest_framework import viewsets
+
+# import local data
+from .serializers import GeeksSerializer
+from .models import GeeksModel
+
+# create a viewset
+class GeeksViewSet(viewsets.ModelViewSet):
+    # define queryset
+    queryset = GeeksModel.objects.all()
+    
+    # specify serializer to be used
+    serializer_class = GeeksSerializer
