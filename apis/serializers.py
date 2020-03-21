6483cb0990c5251b630ddbb17e2697277726df8f
Naveen Arora <naveenarorakrnl@gmail.com>
2020-03-21 11:42:05 +0530

    String Fields in Serializers - Django REST Framework

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,8 +1,15 @@
 #import serializer from rest_framework
 from rest_framework import serializers
 
+class Geeks(object):
+    def __init__(self, name, email, phone_number):
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+
 # create a serializer
-class CommentSerializer(serializers.Serializer):
+class GeeksSerializer(serializers.Serializer):
     # intialize fields
-    field_1 = serializers.BooleanField()
-    field_2 = serializers.NullBooleanField()
+    name = serializers.CharField()
+    email = serializers.EmailField()
+    phone_number = serializers.RegexField("[0-9]{10}")
